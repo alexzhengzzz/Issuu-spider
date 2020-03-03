0cd75767ec44f619fc4f8fa1d45dcd15adc6c0f7
Alex <839910307@qq.com>
2020-03-03 23:56:37 +0800

    修改保存数据的条件

--- IssuuSpider/pipelines.py
+++ IssuuSpider/pipelines.py
@@ -4,19 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
 
 class IssuuspiderPipeline(object):
-    def open_spider(self,spider):
-        self.file = open('results.csv','w')
-    def close_spider(self,spider):
+    def open_spider(self, spider):
+        self.file = open('results.csv', 'w')
+
+    def close_spider(self, spider):
         self.file.close()
+
     def process_item(self, item, spider):
-        # data = str(item['like'][0])
-        # print(data)
         data = str(item['follow']) + ',' + str(item['like']) + ',' + item['detail_url'] + ';\n'
-        print('--------------data-------------------')
-        print(data)
-        print('--------------data-------------------')
-        self.file.write(data)
-        return data
+        if item['follow'] > 0 and item['like'] > 5:
+            self.file.write(data)
+            return data
+        else:
+            raise DropItem("don't satisfy the demand")
--- IssuuSpider/spiders/myspider.py
+++ IssuuSpider/spiders/myspider.py
@@ -4,21 +4,19 @@ from IssuuSpider.items import IssuuspiderItem
 
 
 class MySpider(scrapy.Spider):
+    # MySpider Name
     name = "alex"
     website = "https://issuu.com"
-    state_code = "nBJyxXnRXwn2qJz7in9xh_a6dwKjLxLR6heWBJUy5VyXYa_qVvYRfqdF3YPTPudVMaZITEjB0vDcFf46-M6OIsuxv8pwZgxpyMiPihHMfq7HDTnE_zTkZnE-CoxeyZAp4929WdXK6P8aSfay4RzLngBJ8gpxGD1fDdIdrH2SD3wSTz6ytw=="
+    state_code = "nBJyxXnRXwn2qJz7in9xh_a6dwKjLxLR6heWBJUy5VyXYa_qVvYRfqdF3YPTPudVMaZITEjB0vDcFf46" \
+                 "-M6OIsuxv8pwZgxpyMiPihHMfq7HDTnE_zTkZnE-CoxeyZAp4929WdXK6P8aSfay4RzLngBJ8gpxGD1fDdIdrH2SD3wSTz6ytw== "
+    start_urls = [
+        'https://issuu.com/call/stream/api/publicationsearch/1/0/cont/master-45?state={}&pageSize=20&format=json'.format(
+            state_code)
+    ]
 
-
-    def start_requests(self):
-        urls = [
-            'https://issuu.com/call/stream/api/publicationsearch/1/0/cont/master-45?state={}&pageSize=20&format=json'.format(self.state_code)
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def detail_parse(self, response):
-        with open('myweb.html', 'wb') as f:
-            f.write(response.body)
+    # def detail_parse(self, response):
+    #     with open('myweb.html', 'wb') as f:
+    #         f.write(response.body)
 
     def meta_parse(self, response, detail_url):
         json_like_data = json.loads(response.text)
@@ -28,18 +26,15 @@ class MySpider(scrapy.Spider):
         item['detail_url'] = detail_url
         yield item
 
-
     def parse(self, response):
         json_data = json.loads(response.text)
-        ajax_next_website = self.website + json_data["rsp"]["_content"]["continuation"] + "&pageSize=20&format=json"
-        print(ajax_next_website)
         detail_urls = []
         like_urls = []
         for item in json_data["rsp"]["_content"]['stream']:
             ownerUsername = item['content']['ownerUsername']
             publicationName = item['content']['publicationName']
             # 详情页
-            detail_url = self.website + r"/" + ownerUsername + r"/docs/"+ publicationName
+            detail_url = self.website + r"/" + ownerUsername + r"/docs/" + publicationName
             detail_urls.append(detail_url)
             like_url = r"https://issuu.com/call/document-page/" + ownerUsername + r"/" + publicationName + r"/meta-data"
             like_urls.append(like_url)
@@ -47,6 +42,6 @@ class MySpider(scrapy.Spider):
             yield scrapy.Request(url=like_url,
                                  callback=self.meta_parse,
                                  cb_kwargs=dict(detail_url=detail_url))
-        yield scrapy.Request(url=ajax_next_website,
-                                     callback=self.parse)
-
+        if json_data["rsp"]["_content"]["continuation"] is not None:
+            next_website = self.website + json_data["rsp"]["_content"]["continuation"] + "&pageSize=20&format=json"
+            yield response.follow(next_website, self.parse)
